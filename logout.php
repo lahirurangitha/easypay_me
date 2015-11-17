273587c63f95e92d7db81a57e5713ce5d4424755
lahiru <lahirupathiranalr@gmail.com>
2015-11-17 20:38:18 +0530

    working

---
+++ logout.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lasith Niroshan
+ * Date: 5/23/2015
+ * Time: 1:43 PM
+ */
+require_once 'core/init.php';
+$user = new User();
+$user->logout();
+$_SESSION['isLoggedIn']=false;
+Redirect::to('index.php');
