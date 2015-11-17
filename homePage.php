b53892fadf193c903269f901e3f7981309a23eec
lahiru <lahirupathiranalr@gmail.com>
2015-11-17 20:32:09 +0530

    working

---
+++ homePage.php
@@ -0,0 +1,40 @@
+<?php
+require_once 'core/init.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Home | Page</title>
+    <?php include 'headerScript.php'?>
+</head>
+
+<body>
+
+<?php
+    include 'header.php';
+
+?>
+<div class="backgroundImg container-fluid">
+    <br>
+    <div class="col-lg-offset-5">
+        <img src="images/ucsc.png" height="110px" >
+    </div>
+    <div class="col-lg-offset-3">
+        <img src="images/logo.png" height="150px" >
+    </div>
+    <div class="container col-lg-offset-4">
+        <h2><strong>Online Payment System</strong></h2>
+    </div>
+
+
+
+</div>
+
+<?php
+include "footer.php";
+?>
+
+</body>
+</html>
