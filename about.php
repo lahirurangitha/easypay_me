e7fa6a2cab98d0554ed7909d2703e470441585b0
lahiru <lahirupathiranalr@gmail.com>
2015-11-17 23:31:38 +0530

    Working

---
+++ about.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anjana
+ * Date: 11/2/2015
+ * Time: 1:21 PM
+ */
+
+require_once 'core/init.php';?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title>About | Us</title>
+        <?php include 'headerScript.php'?>
+    </head>
+    <body>
+
+<?php
+include "header.php";
+?>
+<div class="backgroundImg container-fluid">
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="section-heading">Who are we?</h1>
+                <p class="lead section-lead">The easiest way to make payments for UCSC.</p>
+                <p class="section-paragraph">Easy-pay is an online payment system which is in partnership with Dialog Axiata PLC. We facilitate students to make payments to the University of Colombo School of Computing (UCSC) just by a click of a button! The 3 basic types of payments which Easy-pay facilitates are; </p>
+
+                <ul>
+                    <li>UCSC registration fee</li>
+                    <li>Registration fee for a new academic year</li>
+                    <li>Repeat examination fee</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<aside class="image-bg-fixed-height"></aside>
+
+<!-- Content Section -->
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <!--<h1 class="section-heading">Section Heading</h1> -->
+                <h1 class="section-heading"> How to make a payment? </h1>
+                <p class="lead section-lead">No more queues! Transaction within seconds!!</p>
+
+                <p class="section-paragraph"> It's easy like never before! Any student who is going to be or is already an undergraduate of the UCSC can get registered with the Easy-pay system by creating a profile. All a registered student should have to make a payment is a Dialog eZ Cash account or a friend who has a Dialog eZ Cash account. </p>
+            </div>
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container -->
+</section>
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <!--<h1 class="section-heading">Section Heading</h1> -->
+                <h1 class="section-heading"> How to Register with eZ cash?</h1>
+                <img src="images/ez.png"  class="img-responsive img-thumbnail"  alt="register with ez cash" style="width:608px;height:456px;">
+
+            </div>
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container -->
+</section><br>
+    <br>
+</div>
+<?php
+    include "footer.php";
+?>
+    </body>
+</html>
---
+++ contact.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anjana
+ * Date: 11/2/2015
+ * Time: 1:21 PM
+ */
+
+require_once 'core/init.php';?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Contact | Page</title>
+    <?php include 'headerScript.php'?>
+</head>
+<body>
+
+<?php
+include "header.php";
+?>
+<div class="backgroundImg container-fluid">
+<!--<style>-->
+<!---->
+<!--/*.fm {*/-->
+<!--/*    padding-left: 20%;*/-->
+<!--/*    padding-top:5%;*/-->
+<!--/*}*/-->
+<!--/**/-->
+<!--</style>-->
+
+<div class="container">
+    <br>
+    <div class="jumbotron col-lg-6">
+    <form class="form-horizontal" id="contact_form" action="" method="POST" enctype="multipart/form-data">
+
+        <label for="name">Your name:</label>
+        <div class="gap">
+            <input required id="name" class="form-control" name="name" type="text" value="" size="30" />
+        </div>
+
+        <label for="email">Your email:</label>
+        <div class="gap">
+            <input required id="email" class="form-control" name="email" type="email" value="" size="30" />
+        </div>
+
+        <label for="message">Your message:</label>
+        <div class="gap">
+            <textarea required id="message" class="form-control" name="message" rows="7" cols="30"></textarea>
+        </div>
+        <input class="btn btn-primary" id="submit_button" name="submit" type="submit" value="Send email" />
+    </form>
+    </div>
+
+<div class="jumbotron col-lg-5 col-lg-offset-1">
+    <div class="jb-text">
+        <h3><div class="label label-primary">Contact Us </div></h3>
+    <h3><strong>'Easy Pay' Payment system</strong></h3>
+    <h4>35, Philip Gunawardena Mawatha, Colombo 07, Sri Lanka.</h4>
+    <h4>Contact Easypay: 0712364452</h4>
+    <h4>Email: easypaysl@gmail.com</h4>
+    </div>
+</div>
+</div>
+<!--<div class="col-xs-6 col-md-4">-->
+<!--    <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0"width="400" height="443" src="https://maps.google.com/maps?hl=en&q=35 Philip Gunewardena Mawatha, Colombo, Sri Lanka&ie=UTF8&t=roadmap&z=16&iwloc=B&output=embed"><div><small><a href="http://embedgooglemaps.com">embedgooglemaps.com</a></small></div><div><small><a href="http://www.premiumlinkgenerator.com/">free link generators on premiumlinkgenerator.com</a></small></div></iframe>-->
+<!--</div>-->
+</div>
+<?php
+include "footer.php";
+?>
+</body>
+</html>
